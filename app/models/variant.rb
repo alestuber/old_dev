aef97387cce2da966185527b575fda18cae805f8
Mauricio Vieira <mauricio@mercadofresh.com.br>
2015-05-15 14:53:52 -0300

    #18 - Add price validation to variants

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,4 +2,5 @@ class Variant < ActiveRecord::Base
   acts_as_paranoid
 
   belongs_to :supplier
+  validates :price, :numericality => { greater_than_or_equal_to: 0 }
 end
--- test/models/variant_test.rb
+++ test/models/variant_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class VariantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @variant = variants(:variant_cerealbar)
+  end
+
+  def tear_down
+    @variant = nil
+  end
+
+  test "should validate price is greater than 0" do
+    @variant.price = -1
+    assert_not @variant.valid?
+  end
+
+  test "should validate price is 0" do
+    @variant.price = 0
+    assert @variant.valid?
+  end
 end
