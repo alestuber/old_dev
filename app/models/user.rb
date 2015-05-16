bb2672a75cf85cdb8589d65b3fe551d6e95a2b92
Alexandre Stuber <alestuber@gmail.com>
2015-05-16 18:14:13 -0300

    Finish validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :telephone, presence: true, length: { maximum: 11 }
+  validates :date_of_birth, presence: true
+  validates :cpf, presence: true, length: { is: 11 }, uniqueness: true
 end
