557993f7211fb580514e42c7478e8619364c0915
Alexandre Stuber <alestuber@gmail.com>
2015-05-16 15:16:50 -0300

    Add devise tests and fix devise helpers config
    * Add devise tests to user model
    
    * Fix devise helpers. Only for controller tests. It's incorporated on test_helper.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
