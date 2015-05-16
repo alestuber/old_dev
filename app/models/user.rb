f447b8fe85bfbf68d0c76c7303d17c801b4b131e
Alexandre Stuber <alestuber@gmail.com>
2015-05-16 18:42:53 -0300

    Fix phone_field on form and add to accept just numbers on CPF and Phone fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :telephone, presence: true, length: { maximum: 11 }
+  validates :telephone, presence: true, length: { maximum: 11 }, numericality: true
   validates :date_of_birth, presence: true
-  validates :cpf, presence: true, length: { is: 11 }, uniqueness: true
+  validates :cpf, presence: true, length: { is: 11 }, uniqueness: true, numericality: true
 end
