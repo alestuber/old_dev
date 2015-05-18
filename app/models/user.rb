f772e24474dd3376ed2229909a8f0a6dc5797642
Alexandre Stuber <alestuber@gmail.com>
2015-05-18 11:03:08 -0300

    FB login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ActiveRecord::Base
   validates :telephone, presence: true, length: { maximum: 11 }, numericality: true
   validates :date_of_birth, presence: true
   validates :cpf, presence: true, length: { is: 11 }, uniqueness: true, numericality: true
+
+  def self.from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first || User.new
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.first_name = data["first_name"] if user.first_name.blank?
+        user.last_name = data["last_name"] if user.last_name.blank?
+        user.provider = :facebook
+        user.uid = data["id"]
+      end
+    end
+  end
 end
