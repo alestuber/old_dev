29330e3e202804eb970ca521d8cc433030beef3a
Alexandre Stuber <alestuber@gmail.com>
2015-05-27 14:16:53 -0300

    Fix variant display in ActiveAdmin view)

--- app/admin/variant.rb
+++ app/admin/variant.rb
@@ -14,6 +14,34 @@ ActiveAdmin.register Variant do
   #   permitted
   # end
 
+  index do
+    selectable_column
+
+    column :id
+
+    column :sku
+
+    column :ean
+
+    column :price
+
+    column :cost_price
+
+    column :is_master
+
+    column :position
+
+    column :images do |variant|
+      images = variant.images
+      result = []
+        images.each do |image|
+          result << image.to_s
+        end
+      result
+    end
+    actions
+  end
+
   form html: { multipart: true } do |f|
     f.input :images, as: :file, input_html: { multiple: true }
     f.actions
