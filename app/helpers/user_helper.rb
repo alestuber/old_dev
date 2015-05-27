7ea2a03c5959c7ce82b9c1a664934e0338dc2b6b
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-05-27 16:58:28 -0300

    #36 cop -Style/SpaceAfterComma: fixed with --auto-correct

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
   def cpf_formatter(cpf)
-    cpf.gsub!(/(\d{3})(\d{3})(\d{3})(\d{2})/,"\\1.\\2.\\3-\\4")
+    cpf.gsub!(/(\d{3})(\d{3})(\d{3})(\d{2})/, "\\1.\\2.\\3-\\4")
   end
 
   def date_formatter(date)
@@ -9,9 +9,9 @@ module UserHelper
 
   def phone_formatter(phone)
     return nil if phone.length < 8 || phone.length > 12
-    phone.gsub!(/^0+/,'')
-    phone.gsub!(/(\d{1,2})(\d{4,5})(\d{4}$)/,"(\\1) \\2-\\3") if phone.length > 9
-    phone.gsub!(/(\d{4,5})(\d{4}$)/,"\\1-\\2") if phone.length <= 9
+    phone.gsub!(/^0+/, '')
+    phone.gsub!(/(\d{1,2})(\d{4,5})(\d{4}$)/, "(\\1) \\2-\\3") if phone.length > 9
+    phone.gsub!(/(\d{4,5})(\d{4}$)/, "\\1-\\2") if phone.length <= 9
     phone
   end
 end
