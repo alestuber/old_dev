addda0a9f570b1131009a1e0dc1d239d9f9a708c
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-05-27 16:25:40 -0300

    #36 cop Style/PercentLiteralDelimiters: fixed with --auto-correct

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,8 +41,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid email addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn alexandre.stuber@mercadofresh.com.br]
+    valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn alexandre.stuber@mercadofresh.com.br)
 
     valid_addresses.each do |valid_address|
       @user.email = valid_address
@@ -51,7 +51,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid email addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.]
+    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.)
 
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
