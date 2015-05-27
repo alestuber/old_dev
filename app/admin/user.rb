2a24ee4783032df8082e2d1bf57b1750372b218c
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-05-27 13:41:02 -0300

    #36 cop Style/EmptyLinesAroundBlockBody: fixed with --auto-correct

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register User do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -49,5 +48,4 @@ ActiveAdmin.register User do
     end
     actions
   end
-
 end
--- app/admin/variant.rb
+++ app/admin/variant.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Variant do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -17,5 +16,4 @@ ActiveAdmin.register Variant do
     f.input :images, as: :file, input_html: { multiple: true }
     f.actions
   end
-
 end
